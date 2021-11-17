4311ca74e3c60f7f489ed85e86dbb03dae8599e4
Bobbbay <abatterysingle@gmail.com>
2021-11-16 17:53:39 -0800

    [maint] Small formatting touchups.

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,28 +1,23 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import edu.wpi.first.wpilibj.PWMTalonSRX;
-import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.RobotContainer;
-import edu.wpi.first.wpilibj.Solenoid;
 
 public class DriveSubsystem extends SubsystemBase {
     private final WPI_TalonSRX right1 = new WPI_TalonSRX(Constants.Drive.rightDrive1);
     private final WPI_TalonSRX right2 = new WPI_TalonSRX(Constants.Drive.rightDrive2);
-    SpeedControllerGroup right = new SpeedControllerGroup(right1, right2);
+    private final SpeedControllerGroup right = new SpeedControllerGroup(right1, right2);
 
     private final WPI_TalonSRX left1 = new WPI_TalonSRX(Constants.Drive.leftDrive1);
     private final WPI_TalonSRX left2 = new WPI_TalonSRX(Constants.Drive.leftDrive2);
-    SpeedControllerGroup left = new SpeedControllerGroup(left1, left2);
+    private final SpeedControllerGroup left = new SpeedControllerGroup(left1, left2);
 
-    DifferentialDrive drive = new DifferentialDrive(right, left);
+    private final DifferentialDrive drive = new DifferentialDrive(right, left);
 
-    public DriveSubsystem() {
-    }
+    public DriveSubsystem() {}
 
     public void arcadeDrive(double speed, double rotation) {
         drive.arcadeDrive(speed, rotation);
